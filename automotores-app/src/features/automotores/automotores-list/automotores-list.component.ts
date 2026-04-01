1ad05dd829e51b9f3c922dee3fcaeb26046ce966
Candelaria <ferrari.candelaria@gmail.com>
2026-04-01 15:58:48 -0300

    feat(listado): Agregue filtrado de búsqueda y mejore acciones de tabla

--- automotores-app/src/features/automotores/automotores-list/automotores-list.component.ts
+++ automotores-app/src/features/automotores/automotores-list/automotores-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -9,19 +10,26 @@ export interface Automotor {
   owner: string;
   cuit: string;
   fabrication: string;
- }
+}
 
 
 @Component({
   selector: 'app-automotores-list',
-  imports: [MatTableModule, MatIconModule, MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './automotores-list.component.html',
   styleUrl: './automotores-list.component.scss'
 })
 export class AutomotoresListComponent {
   displayedColumns: string[] = ['domain', 'owner', 'cuit', 'fabrication', 'actions'];
 
-  dataSource: Automotor[] = [
+  searchValue: string = '';
+
+  originalData: Automotor[] = [
     {
       domain: 'ABC12345',
       owner: 'Juan Perez',
@@ -33,5 +41,33 @@ export class AutomotoresListComponent {
       owner: 'Maria Gomez',
       cuit: '27333444555',
       fabrication: '202112'
+    },
+    {
+      domain: 'DFG12345',
+      owner: 'Juan Perez',
+      cuit: '20304050607',
+      fabrication: '202201'
+    },
+    {
+      domain: 'HIJ6789',
+      owner: 'Maria Gomez',
+      cuit: '27333444555',
+      fabrication: '202112'
     }
-  ]; }
+  ];
+  dataSource = [...this.originalData];
+
+  applyFilter(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    .toLowerCase()
+    .trim();
+
+    this.searchValue = value;
+
+  this.dataSource = this.originalData.filter(item =>
+    item.domain.toLowerCase().includes(value) ||
+    item.cuit.toLowerCase().includes(value)
+  );
+  }
+
+}
